da8b05ec4b8d583cd7fd5c3ce4d6ab00056bf0f4
Bryan Calupitan <bryan@luxeys.com>
2019-04-27 23:09:28 +0900

    pwa fixes

--- todolist/static/js/serviceworker.js
+++ todolist/static/js/serviceworker.js
@@ -1,10 +1,17 @@
-var staticCacheName = 'djangopwa-v1';
+var staticCacheName = 'todopwa-v1';
 
 self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open(staticCacheName).then(function(cache) {
       return cache.addAll([
-        '/base_layout'
+        '/base_layout',
+        '/static/js/idb.js',
+        '/static/js/idbop.js',
+        '/static/css/main.css',
+        '/manifest.json',
+        '/static/icon/todo.png',
+        '/static/images/todoapp.png',
+        '/serviceworker.js'
       ]);
     })
   );
