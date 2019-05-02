9811868878a3cb9fb43f338add470160c9c2febb
gabriel-tessier <gabriel-tessier@users.noreply.github.com>
2019-05-02 11:43:30 +0900

    Update the views
    Add comment and rename the getdata to get_data

--- todolist/views.py
+++ todolist/views.py
@@ -7,12 +7,14 @@ from .forms import TodoLisForm
 
 
 def index(request):
+    """Top page."""
     form = TodoLisForm()
     todos = TodoList.objects.all()
     return render(request, 'todolist/index.html', {'todos': todos, 'form': form})
 
 
 def add(request):
+    """Add a todo with a form."""
     if request.method == 'POST':
         form = TodoLisForm(request.POST)
 
@@ -25,12 +27,14 @@ def add(request):
 
 
 def delete(request, todo_id):
+    """Delete a todo."""
     todo = TodoList.objects.get(pk=todo_id)
     todo.delete()
     return redirect('todolist:index')
 
 
 def done(request, todo_id):
+    """Mark the task as done."""
     todo = TodoList.objects.get(pk=todo_id)
     # toggle
     todo.done = not todo.done
@@ -39,11 +43,13 @@ def done(request, todo_id):
 
 
 def base_layout(request):
+    """Duplicate with index???"""
     form = TodoLisForm()
     return render(request, 'todolist/index.html', {'form': form})
 
 
-def getdata(request):
+def get_data(request):
+    """Json list of result."""
     results = TodoList.objects.all()
     jsondata = serializers.serialize('json',results)
     return HttpResponse(jsondata)
